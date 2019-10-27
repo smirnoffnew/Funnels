b6079227fac4a27673f15d6ca028fffa9197f57c
victorpilipenko <thejustvic@gmail.com>
2019-10-27 14:07:54 +0200

    Facebook Pixel Code

--- FrontEnd/funnelsmapFrontend/src/config.js
+++ FrontEnd/funnelsmapFrontend/src/config.js
@@ -2,5 +2,5 @@
 // export const API_URL = window.location.protocol === 'https:' ? 'https://api.funnelsmap.com' : 'http://api.funnelsmap.com';
 // export const API_URL = 'https://api.funnelsmap.com';
 // export const API_URL = 'https://funnelsmapbackend.qbex.io';
-export const API_URL = 'http://localhost:9001';
-// export const API_URL = 'http://ec2-3-124-11-139.eu-central-1.compute.amazonaws.com:9001';
+// export const API_URL = 'http://localhost:9001';
+export const API_URL = 'http://ec2-3-124-11-139.eu-central-1.compute.amazonaws.com:9001';
