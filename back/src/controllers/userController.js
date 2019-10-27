7fd3260c606f43f49d13ffa99690c4b808871010
smirnoffnew <smirnoffnew@gmail.com>
2019-10-27 21:07:49 +0200

    Kostia fix

--- back/src/controllers/userController.js
+++ back/src/controllers/userController.js
@@ -2,31 +2,29 @@ const mongoose = require("mongoose");
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const activeCampaignApi = require('activecampaign-api');
+const AC = require('activecampaign-rest');
 
 const User = require('../models/user.js');
 const Profile = require('../models/profile.js');
 const sendEmail = require('../libs/sendEmail.js');
 const validateEmail = require('../libs/validateEmail.js');
 const keygen = require('../libs/keygen.js');
-const AC = require('activecampaign-rest')
 
 module.exports = {
     signUp: async function (req, res) {
 
-        let user = {};
-        let profile = {};
-        let token = {};
-
-        user = await new User({
+        const user = await new User({
             _id: new mongoose.Types.ObjectId(),
             ...req.body
         });
 
+        console.log(user)
+
         await user.email.toLowerCase();
 
-        user
-            .save()
-            .then(user => new Profile({
+        user.save()
+            .then(doc => {
+                const profile = new Profile({
                     _id: new mongoose.Types.ObjectId(),
                     password: user.password,
                     limited: user.limited,
@@ -34,37 +32,65 @@ module.exports = {
                     firstName: user.firstName,
                     accountName: user.accountName,
                     description: req.body.description || null
-                })
-            )
-            .then( profile => profile.save() )
-            .then(savedProfile => {
-                profile = savedProfile;
-                token = jwt.sign({profile, userId: user._id}, process.env.SECRET, {expiresIn: '99999d'});
-
-                return new activeCampaignApi.ApiClient({
-                    accountName: process.env.CAMPAING_ACCOUNT_NAME,
-                    key: process.env.CAMPAING_ACCOUNT_KEY
-                })
-                .call('contact_add', {}, 'POST', {
-                    email: profile.email,
-                    first_name: profile.firstName,
-                    tags: profile.description,
-                    'p[15]': process.env.LISTID
-                })
-            })
-            .then(response => {
-                return res.status(200).json({
-                    data: {
-                        _id: profile._id,
-                        firstName: profile.firstName,
-                        limited:profile.limited,
-                        email: profile.email,
-                        description: profile.description,
-                        photoUrl: profile.photoUrl,
-                        accountName: profile.accountName
-                    },
-                    token: `Bearer ${token}`
                 });
+
+                profile.save()
+                    .then(profile => {
+                        const token = jwt.sign({
+                            profile,
+                            userId: doc._id
+                        }, process.env.SECRET, {expiresIn: process.env.TOKEN_EXPIRES});
+
+                        new activeCampaignApi.ApiClient({
+                            accountName: process.env.CAMPAING_ACCOUNT_NAME,
+                            key: process.env.CAMPAING_ACCOUNT_KEY
+                        })
+                            .call('contact_add', {}, 'POST', {
+                                email: profile.email,
+                                first_name: profile.firstName,
+                                tags: profile.description,
+                                'p[15]': process.env.LISTID
+                            })
+                            .then(response => {
+                                console.log(response);
+                                res.status(200).json({
+                                    data: {
+                                        _id: profile._id,
+                                        firstName: profile.firstName,
+                                        limited: profile.limited,
+                                        email: profile.email,
+                                        description: profile.description,
+                                        photoUrl: profile.photoUrl,
+                                        accountName: profile.accountName
+                                    },
+                                    token: `Bearer ${token}`,
+                                    responseApi: response,
+                                });
+                                res
+                                    .status(200)
+                                    .json({
+                                        res_code: response.result_code,
+                                        fullApiResponse: response,
+                                    })
+                            })
+                            .catch(err => {
+                                console.log('activeCampaignApi Error', err);
+                                res
+                                    .status(404)
+                                    .json({
+                                        message: 'activeCampaignApi error',
+                                        data: err,
+                                    })
+                            })
+
+
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({
+                            errors: err,
+                        });
+                    })
             })
             .catch(err => {
                 console.log(err);
@@ -72,30 +98,26 @@ module.exports = {
             })
     },
     signIn: async function (req, res) {
-
         await req.body.email.toLowerCase();
-
         const emailFromUser = await validateEmail(req.body.email);
-
+        let apiResponse = '';
         if (emailFromUser) {
-            User
-                .findOne({email: req.body.email})
+            User.findOne({email: req.body.email})
                 .exec()
                 .then(user => {
 
                         if (user && bcrypt.compareSync(req.body.password, user.password)) {
-
-                                Profile.findOne({email: req.body.email})
+                            Profile.findOne({email: req.body.email})
                                 .exec()
                                 .then(profile => {
-                    
+                                    if (profile) {
                                         let today = new Date();
-                                        let date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear()
+                                        let date = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
 
                                         let contact = new AC.Contact({
                                             'url': 'https://vladhuntyk.activehosted.com',
                                             'token': process.env.CAMPAING_ACCOUNT_KEY
-                                        })
+                                        });
 
                                         let payload = {
                                             'email': profile.email,
@@ -108,31 +130,38 @@ module.exports = {
                                                     'value': date
                                                 },
                                             ]
-                                        }
-
+                                        };
                                         contact.sync(payload, (err, res) => {
-                                                if (err) {
-                                                    console.log("error", err)
-                                                }
-                                        })
-                                   
-                                    if (profile) {
+                                            if (err) {
+                                                console.log("AC error", err)
+                                                res
+                                                    .status(404)
+                                                    .json({
+                                                        message: 'AC error',
+                                                        data: err,
+                                                    })
+                                            }
+                                            console.log(res);
+                                        });
+                                        const token = jwt.sign({
+                                            profile,
+                                            userId: user._id
+                                        }, process.env.SECRET, {expiresIn: process.env.TOKEN_EXPIRES});
 
-                                        const token = jwt.sign({profile,userId:user._id}, process.env.SECRET, {expiresIn: process.env.TOKEN_EXPIRES});
-
-                                        res.status(200).json({
-                                            data: {
-                                                _id: profile._id,
-                                                firstName: profile.firstName,
-                                                email: profile.email,
-                                                description: profile.description,
-                                                photoUrl: profile.photoUrl,
-                                                accountName: profile.accountName
-                                            },
-                                            token: `Bearer ${token}`
-                                        })
+                                        res
+                                            .status(200)
+                                            .json({
+                                                data: {
+                                                    _id: profile._id,
+                                                    firstName: profile.firstName,
+                                                    email: profile.email,
+                                                    description: profile.description,
+                                                    photoUrl: profile.photoUrl,
+                                                    accountName: profile.accountName
+                                                },
+                                                token: `Bearer ${token}`,
+                                            })
                                     } else {
-
                                         const profile = new Profile({
                                             _id: new mongoose.Types.ObjectId(),
                                             password: user.password,
@@ -140,10 +169,12 @@ module.exports = {
                                             firstName: user.firstName,
                                             accountName: user.accountName
                                         });
-
                                         profile.save()
                                             .then(profile => {
-                                                const token = jwt.sign({profile,userId:user._id}, process.env.SECRET, {expiresIn: process.env.TOKEN_EXPIRES});
+                                                const token = jwt.sign({
+                                                    profile,
+                                                    userId: user._id
+                                                }, process.env.SECRET, {expiresIn: process.env.TOKEN_EXPIRES});
                                                 res.status(200).json({
                                                     data: profile,
                                                     token: `Bearer ${token}`
@@ -161,9 +192,9 @@ module.exports = {
                                     res.status(500).json({err});
                                 });
 
-                         } else {
-                             res.status(400).json({message: "User not found or wrong password"});
-                         }
+                        } else {
+                            res.status(400).json({message: "User not found or wrong password"});
+                        }
                     }
                 )
                 .catch(err => {
@@ -171,7 +202,7 @@ module.exports = {
                     res.status(500).json({error: err.message});
                 });
         } else {
-            res.status(400).json({message: "email is not correct"});
+            res.status(400).json({message: "email isn't correct"});
         }
 
     },
@@ -197,12 +228,19 @@ module.exports = {
         User.findOne({email: emailReset})
             .exec()
             .then(user => {
+
                 if (user) {
                     const token = jwt.sign({email: user.email}, process.env.SECRET_LETTER, {expiresIn: process.env.TOKEN_EXPIRES_LETTER});
-                    const options = sendEmail.getMailoptions(user.email, user.firstName,token);
-                    sendEmail.transporter.sendMail(options, function (err, info) {
+                    const options = sendEmail.getMailoptions(user.email, user.firstName, token);
+                    sendEmail.transporter.sendMail(options, (err, info) => {
                         if (err) {
-                            console.log(err);
+                            console.log (err);
+                            res
+                                .status(500)
+                                .json({
+                                    message: "Email not send!",
+                                    data: err
+                                })
                         } else {
                             res.status(201).json({
                                 message: "Email sended!",
@@ -211,7 +249,7 @@ module.exports = {
                         }
                     });
                 } else {
-                    res.status(404).json({message: "email doesn't exists"});
+                    res.status(404).json({message: "email doesn't exists in DB"});
                 }
             })
             .catch(err => {
@@ -253,10 +291,10 @@ module.exports = {
 
     },
     generateSecretKeys: async function (req, res) {
-         res.json({
-             secretKey: keygen(20),
-             secretKeyEmail: keygen(20)
-         });
+        res.json({
+            secretKey: keygen(20),
+            secretKeyEmail: keygen(20)
+        });
     },
 
 
