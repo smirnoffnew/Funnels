e9a546c7930a3482a23bf59ac03b0b47b68ae794
victorpilipenko <thejustvic@gmail.com>
2019-10-31 16:59:58 +0200

    remove src and index from from the root

--- back/src/libs/keygen.js
+++
@@ -1,7 +0,0 @@
-module.exports = (keyLength) => {
-    let key = "", characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    for (let i = 0; i < keyLength; i++) {
-        key += characters.substr(Math.floor((Math.random() * characters.length) + 1), 1);
-    }
-    return key;
-};
--- back/src/libs/lastModified.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(req, res, next) {
-    res.setHeader('Last-Modified', (new Date()).toUTCString());
-    next();
-};
